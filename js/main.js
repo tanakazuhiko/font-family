57293ef4ef9d18888263f888d00beaed80393aab
田中一彦 <kzhk.tnk@gmail.com>
2019-12-28 17:18:21 +0900

    githu

--- js/main.js
+++ js/main.js
@@ -6,19 +6,54 @@
 var path = "../setting.json";
 var api_url = "https://www.googleapis.com/webfonts/v1/webfonts?key=";
 var css_url = "https://fonts.googleapis.com/css?display=swap&family=";
-var string1 = "abcdefghijklmnopqrstuvwxyz";
-var string2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var string1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var string2 = "abcdefghijklmnopqrstuvwxyz";
 var string3 = "1234567890";
 var string4 = '!"#$%&' + "'()*+,-./:;<=>?@[\]^_`{|}~";
 var string5 = "あいうえおかきくけこ";
 var string6 = "アイウエオカキクケコ";
 var string7 = "祇辻飴葛蛸鯖鰯噌庖箸";
+var br = "<br>";
+var quote = "'";
+var double_quote = '"';
+var space = " ";
+var plus = "+";
+var none = "";
+var pre_c = "c_";
+var style_start = '<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=';
+var style_end = '">';
+var css_start = '.wf-';
+var css_mid = ' { font-family: "';
+var css_end = '"; }';
+var div_start = "<div>";
+var div_end = "</div>";
+var div_src = "<div class='src'>";
+var span_start = "<span>";
+var span_end = "</span>";
+var pre_start = "<pre>";
+var pre_end = "</pre>";
+var span_head = "<span class='head'>";
+var span_small = "<span class='small'>";
+var span_normal = "<span class='normal'>";
+var span_bold = "<span class='bold'>";
+var span_italic = "<span class='italic'>";
+var span_bold_italic = "<span class='bold italic'>";
+var str_small = "[font-weight: small]"
+var str_normal = "[font-weight: normal]"
+var str_bold = "[font-weight: bold]"
+var str_italic = "[font-style: italic]"
+var str_bold_italic = "[font-weight: bold; font-style: italic]"
+var str_html = "[html]";
+var str_css = "[css]";
 var offset = 50;
 var offset_x = 20;
 var start_x = 450;
-var duration_sort = 1000;
+var duration_sort = 1600;
 var zoom_from = 0.01;
 var zoom_to = 10000;
+var weight_light = 100;
+var weight_normal = 400;
+var weight_bold = 900;
 var key;
 var width, height;
 var svg, zoom;
@@ -26,7 +61,7 @@ var x, xAxis, gX, y, yAxis, gY;
 var device, google;
 var dataset = [];
 var g_family, family, g_sample, sample, help_msg;
-var e_tab;
+var e_tab, e_family, e_sample, e_fade, e_msg;
 var count_array = [];
 
 // api key
@@ -35,42 +70,48 @@ d3.json(path).then(
         // google font
         d3.json(api_url + data.key).then(
             function(google){
+                // init
                 init(google.items);
+                // axis
                 axis();
-                // dataset = device.concat(google.items);
-                dataset = filter(google.items, "A");
+                // dataset
+                dataset = filter(google.items, string2.slice(0,1));
+                // css
                 css(dataset);
+                // draw
                 draw(dataset);
-
+                // zoom
+                initZoom();
+                // reset
+                resetted();
+                // family
+                listenFamily();
                 // tab
                 e_tab.forEach(function(item, index) {
                     item.addEventListener("change", function() {
-                        resetted();
+                        remove();
                         if(this.id == "help") {
                             help(dataset);
+                            resetted();
                         } else {
                             dataset = filter(google.items, this.id);
                             css(dataset);
                             draw(dataset);
+                            initZoom();
+                            resetted();
+                            listenFamily();
                         }
                     });
                 });
+                // fade
+                e_fade.onclick = function() {
+                    e_fade.style.visibility = "hidden";
+                    e_msg.style.visibility = "hidden";
+                }
             }
         )
     }
 );
-
-// filter
-function filter(items, key) {
-    var result = [];
-    items.forEach(function(v, i){
-        var str = v.family;
-        if(str.toUpperCase().startsWith(key.toUpperCase())) {
-            result.push(v);
-        }
-    });
-    return result;
-}
 
 // init
 function init(items) {
@@ -80,6 +121,8 @@ function init(items) {
 
     // element
     e_tab = document.querySelectorAll(".tab-switch");
+    e_fade = document.getElementById("fade_layer");
+    e_msg = document.getElementById("msg");
 
     // count
     items.forEach(function(v, i) {
@@ -91,7 +134,7 @@ function init(items) {
         }
     });
 
-    var alphabet = string2.split("");
+    var alphabet = string1.split("");
     for(var i = 0; i < alphabet.length; i++) {
         var elm = document.getElementById(alphabet[i]);
         var count = !count_array[alphabet[i]] ? 0 : count_array[alphabet[i]];
@@ -106,14 +149,24 @@ function init(items) {
     .attr("class", "svg grabbable")
     .attr("width", width)
     .attr("height", height);
+}
 
-    // zoom
+// initZoom
+function initZoom() {
     zoom = d3
     .zoom()
     .scaleExtent([zoom_from, zoom_to])
     .on("zoom", zoomed);
 
     svg.call(zoom);
+
+    var zoom2 = d3
+    .zoomIdentity
+    .translate(width / 2, height / 2)
+    .scale(10)
+    .translate(-7000, -1000);
+
+    svg.call(zoom.transform, zoom2);
 }
 
 // axis
@@ -178,7 +231,8 @@ function draw(dataset) {
     .enter()
     .append("text")
     .attr("class", "family")
-    .style("font-family", function(d) { return d.family; })
+    .attr("id", function(d) { return d.family; })
+    .style("font-family", function(d) { return quote + d.family + quote; })
     .attr("x", function(d) { return start_x; })
     .attr("y", function(d, i) { return (i+1) * offset; })
     .text(function(d) {
@@ -200,10 +254,11 @@ function draw(dataset) {
     .enter()
     .append("text")
     .attr("class", "sample")
-    .style("font-family", function(d) { return d.family; })
+    .attr("id", function(d) { return pre_c + d.family; })
+    .style("font-family", function(d) { return quote + d.family + quote; })
     .attr("x", function(d) { return start_x + offset; })
     .attr("y", function(d, i) { return (i+1) * offset; })
-    .text(function(d) { return string2 + " " + string1 + " " + string3; });
+    .text(function(d) { return string1 + space + string2 + space + string3; });
 }
 
 // zoom
@@ -221,14 +276,69 @@ function resetted() {
     svg
     .transition()
     .duration(duration_sort)
-    .call(
-        zoom.transform,
-        d3.zoomIdentity
-    );
-    // remove
+    .call(zoom.transform, d3.zoomIdentity);
+}
+
+// remove
+function remove() {
     font_family.remove();
     font_sample.remove();
     if(help_msg) help_msg.remove();
+    e_fade.style.visibility = "hidden";
+    e_msg.style.visibility = "hidden";
+}
+
+// family
+function listenFamily() {
+    string = string1 + br + string2 + br + string3;
+    e_family = document.querySelectorAll(".family");
+    e_family.forEach(function(item, index) {
+        item.addEventListener("click", function() {
+            setMsg(this.id);
+        });
+    });
+
+    e_sample = document.querySelectorAll(".sample");
+    e_sample.forEach(function(item, index) {
+        item.addEventListener("click", function() {
+            setMsg(this.id.replace(pre_c, none));
+        });
+    });
+}
+
+// msg
+function setMsg(family) {
+    var style = style_start + family.replace(/ /g, plus) + style_end;
+    style = style.replace(/</g,'&lt;').replace(/>/,'&gt;');
+    var lower = family.replace(/ /g, none).toLowerCase();
+    var str = div_start + family + div_end;
+    str += span_head + str_normal + span_end + br;
+    str += span_normal + string + span_end + br;
+    str += span_head + str_bold + span_end + br;
+    str += span_bold + string + span_end + br;
+    str += span_head + str_italic + span_end + br;
+    str += span_italic + string + span_end + br;
+    str += span_head + str_bold_italic + span_end + br;
+    str += span_bold_italic + string + span_end + br;
+    str += div_src + style + br;
+    str += css_start + lower + css_mid + family + css_end + div_end;
+
+    e_fade.style.visibility = "visible";
+    e_msg.style.visibility = "visible";
+    e_msg.style.fontFamily = quote + family + quote;
+    e_msg.innerHTML = str;
+}
+
+// filter
+function filter(items, key) {
+    var result = [];
+    items.forEach(function(v, i){
+        var str = v.family;
+        if(str.toUpperCase().startsWith(key.toUpperCase())) {
+            result.push(v);
+        }
+    });
+    return result;
 }
 
 // help
@@ -241,12 +351,14 @@ function help(dataset) {
         "ダブルクリック：ズーム・イン",
         "シフト＋ダブルクリック：ズーム・アウト",
         "ホイール：ズーム・イン／アウト",
+        "クリック：フォントバリエーション表示",
         "",
         "[usage]",
         "drag: move",
         "double click: zoom in",
         "shift + double click: zoom out",
         "scroll: zoom in/out",
+        "click: show font variation",
         "",
         "[HOMAGE TO A TYPEFACE]",
         "designed and developed by tanakazuhiko, 2019",
